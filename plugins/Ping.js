2d6592a41c274ea4c1f976e55fddb95c68f9ce79
Shaviya Dev / <cybernoxx7489@gmail.com>
2026-06-10 13:55:11 +0530

    Update Ping.js

--- plugins/Ping.js
+++ plugins/Ping.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, reply }) => {
         const ping = Date.now() - start;
 
         await conn.sendMessage(from, {
-            text: `🏓 *Pong!*\n⚡ Response: ${ping}ms\n\n${config.BOT_FOOTER |||}`,
+            text: `🏓 *Pong!*\n⚡ `Response: ${ping}ms``,
             edit: pong.key
         });
     } catch (e) {
